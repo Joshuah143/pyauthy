68da3c94683e1ec8d26d22df32a536476f1b398b
Joshua <83801098+Joshuah143@users.noreply.github.com>
2021-06-27 15:55:32 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import smtplib
+import twilio
+
+
+class auth:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def emailaith():
+        pass
+
+    @staticmethod
+    def textauth():
+        pass
